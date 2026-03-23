981c72a76aea813e824de4f2e8439fc0ebb44b33
Andark11 <soca9906@gmail.com>
2026-03-23 15:26:53 -0600

    Cambio de paradigma mixto para socket en ports

--- src/Clase_4/1-nmap.py
+++
@@ -1,5 +0,0 @@
-import socket
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.settimeout(0.5) # tiempo de espera 
-    if s.connect_ex(("google.com", 443)) == 0:
-        print("puerto abierto")
